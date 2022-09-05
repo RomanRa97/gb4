1636f3e293d72302c214619989298dc4ab68612f
roman <roraf@mail.ru>
2022-09-05 18:58:50 +0500

    Homework lesson 4

---
+++ Homework1/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите степень");
+int b = int.Parse(Console.ReadLine());
+
+int value = 1;
+for(int i=1; i <= b; i++)
+{
+    value = value * a;
+}
+
+Console.WriteLine(value);
---
+++ Homework2/Program.cs
@@ -0,0 +1,24 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int counter = 0;
+
+int lenght (int number)
+{
+    int num_len = 1;
+    while ((number /= 10) >=1) ++num_len;
+    return num_len;      
+}
+int len = lenght(number);
+
+int [] array = new int[len];
+int i = 0;
+int tmp = number;
+
+while (i<len)
+{
+    array[i] = tmp % 10;
+    tmp = tmp / 10;
+    if (array[i] != 0) 
+        if (number % array[i] == 0) counter += array[i];
+    i++;
+}
+Console.WriteLine(counter);
